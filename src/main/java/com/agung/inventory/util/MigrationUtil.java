bdf8cba93367726fcea2d6c0c43f9fb71d119c3d
Agung Pramono <agungpermadi13@gmail.com>
2020-12-14 14:27:11 +0700

    implementasi db migration denga flyway migration

--- src/main/java/com/agung/inventory/util/MigrationUtil.java
+++ src/main/java/com/agung/inventory/util/MigrationUtil.java
@@ -3,8 +3,7 @@ package com.agung.inventory.util;
 import org.flywaydb.core.Flyway;
 
 public class MigrationUtil {
-
-    private static final String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+    
     private static final String jdbcUrl = "jdbc:mysql://localhost:3306/inventory?serverTimezone=Asia/Jakarta";
     private static final String jdbcUsername = "admin";
     private static final String jdbcPassword = "admin123";
